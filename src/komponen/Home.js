8dc94b2da746648d3d9873257b6c5e5e3a6a1f62
Riki-Toro <rikitoro22@gmail.com>
2021-10-09 18:21:52 +0700

    menampilkan footer

--- src/komponen/Home.js
+++ src/komponen/Home.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 class Home extends Component {
     render() {
@@ -7,6 +8,7 @@ class Home extends Component {
             <div>
                 <Navbar />
                 <h1>Nama Saya Riki Widiantoro</h1>
+                <Footer />
             </div>
         )
     }
