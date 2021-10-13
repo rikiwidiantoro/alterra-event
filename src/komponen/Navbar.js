99aab32357198110a687b411bf600b60ea5b245c
Riki-Toro <rikitoro22@gmail.com>
2021-10-13 18:42:23 +0700

    update

--- src/komponen/Navbar.js
+++ src/komponen/Navbar.js
@@ -9,21 +9,13 @@ const Navbar = () => {
             <nav>
                 <ul>
                     <h2>Riki Widiantoro</h2>
-                    <li><a href={Home}>Home</a></li>
-                    <li><a href='Fiturr'>Fitur 1</a></li>
+                    <li><a href=''>Home</a></li>
+                    <li><a href=''>Fitur 1</a></li>
                 </ul>
                 <button id='btn'>Keluar</button>
             </nav>
         </div>
-
-        
-)
-// function klik() {
-//         const btn = document.getElementById('btn');
-//         btn.click(function() {
-//             alert('Anda berhasil keluar!');
-//         });
-//     }
-// }
+    )
+}
 
 export default Navbar;
