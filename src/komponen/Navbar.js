8588b8d44101d96cad5c8200342bd58848e8d0fe
Riki-Toro <rikitoro22@gmail.com>
2021-10-13 18:38:43 +0700

    error

--- src/komponen/Navbar.js
+++ src/komponen/Navbar.js
@@ -12,18 +12,18 @@ const Navbar = () => {
                     <li><a href={Home}>Home</a></li>
                     <li><a href='Fiturr'>Fitur 1</a></li>
                 </ul>
-                <button id='btn' onClick={state.klik()}>Keluar</button>
+                <button id='btn'>Keluar</button>
             </nav>
         </div>
 
         
 )
-function klik() {
-        const btn = document.getElementById('btn');
-        btn.click(function() {
-            alert('Anda berhasil keluar!');
-        });
-    }
-}
+// function klik() {
+//         const btn = document.getElementById('btn');
+//         btn.click(function() {
+//             alert('Anda berhasil keluar!');
+//         });
+//     }
+// }
 
 export default Navbar;
