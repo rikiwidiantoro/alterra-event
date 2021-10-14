140191904a54d724ac2c3e09d8366428632665bf
Riki-Toro <rikitoro22@gmail.com>
2021-10-14 18:40:08 +0700

    gagal

--- src/komponen/script.js
+++ src/komponen/script.js
@@ -4,11 +4,10 @@ class Script extends Component {
     render() {
         return(
             <div>
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
                 <script>
-                    $(document).ready(function() {
-                        console.log('ok')
-                    });
+                    document.getElementById('#btn').click(function() {
+                        alert('Anda berhasil keluar!')
+                    })
                 </script>
             </div>
         )
