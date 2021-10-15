34034603dc222a61a958fcbbaec3705eb97825bd
Riki-Toro <rikitoro22@gmail.com>
2021-10-15 19:15:38 +0700

    update

--- src/komponen/script.js
+++ src/komponen/script.js
@@ -4,11 +4,11 @@ class Script extends Component {
     render() {
         return(
             <div>
-                <script>
+                
                     document.getElementById('#btn').click(function() {
                         alert('Anda berhasil keluar!')
                     })
-                </script>
+                
             </div>
         )
     }
